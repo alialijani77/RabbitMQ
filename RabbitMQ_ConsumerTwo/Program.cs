46c13a61f7c69730c76e7d0ef802f9600378b1cf
ali <alialijani1377@gmail.com>
2025-06-22 15:07:34 +0330

    CosumerTwo is fixed

--- RabbitMQ_ConsumerTwo/Program.cs
+++ RabbitMQ_ConsumerTwo/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+Console.WriteLine("Cosumer_Two");
+Console.WriteLine("====================================");
+
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "firstexchange", ExchangeType.Direct);
+
+
+await channel.QueueDeclareAsync("queue_two");
+
+await channel.QueueBindAsync(queue: "queue_two", exchange: "firstexchange", routingKey: "routingkey2");
+
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+	var body = ea.Body.ToArray();
+	var message = Encoding.UTF8.GetString(body);
+	Console.WriteLine($" [x] Received {message}");
+};
+
+await channel.BasicConsumeAsync("queue_two", true, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
